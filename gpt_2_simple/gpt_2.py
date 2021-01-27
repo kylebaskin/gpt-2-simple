cb7b40030d2397f11a46ece74941e9f8f75b9d66
Kyle Baskin <>
2021-01-27 18:18:01 -0500

    tset

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -87,9 +87,9 @@ def download_gpt2(model_dir='models', model_name='anton-v1'):
     sub_dir = sub_dir.replace('\\', '/')  # needed for Windows
     print('directory has been found or created')
 
-    for file_name in ['checkpoint', 'encoder.json', 'hparams.json',
+    for file_name in ['encoder.json', 'hparams.json',
                       'model.ckpt.data-00000-of-00001', 'model.ckpt.index',
-                      'model.ckpt.meta', 'vocab.bpe']:
+                      'model.ckpt.meta', 'vocab.bpe', 'checkpoint']:
         download_file_with_progress(url_base="https://anton.summitwebsolutions.com/gpt-2/",
                                     sub_dir=sub_dir,
                                     model_name=model_name,
